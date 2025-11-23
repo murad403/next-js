ce58bea9a0026473c8485aa41444e751bda3dbf2
murad403 <mdm78774@gmail.com>
2025-11-23 11:25:44 +0600

    loading and not found page

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadingPage = () => {
+  return (
+    <div>
+      <h2 className='text-blue-500 text-xl font-bold'>Loading....</h2>
+    </div>
+  )
+}
+
+export default LoadingPage
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h2 className='text-red-500 text-center text-2xl'>Page not found</h2>
+    </div>
+  )
+}
+
+export default NotFoundPage
